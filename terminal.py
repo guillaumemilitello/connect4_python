d83e121104d04c431b755d2bb640a81922fa9e8d
guglielmilo <guillaume.militello@gmail.com>
2016-03-28 23:17:36 +0200

    Update minimum terminal size info on sys.exit

--- terminal.py
+++ terminal.py
@@ -21,7 +21,7 @@ class color:
     RED_H    =  5
     YELLOW_H =  6
 
-# MAC OS X mapping
+# Unix mapping
 class keyboard:
     LEFT_KEY = 260
     RIGHT_KEY = 261
@@ -37,18 +37,18 @@ class keyboard:
 
 def init():
     height_term, width_term = terminalsize.get_terminal_size()
-    height_min = board.col_height * board.height + 2 + 8
+    height_min = board.col_height * board.height + 2 + 9
     width_min = board.col_width * board.width + 2 + 5
     if height_term < height_min or width_term < width_min:
         # resize the terminal to fit the minimum size to display the connect4 before exit
         sys.stdout.write("\x1b[8;{h};{w}t".format(h=max(height_min, height_term), w=max(width_min, width_term)))
-        sys.exit('Please resize your terminal [%d%s%d] (minimum required 45x28)' %(width_term, 'x', height_term))
+        sys.exit('Please resize your terminal [%d%s%d] (minimum required %d%s%d)' %(width_term, 'x', height_term, width_min, 'x', height_min))
     stdscr = curses.initscr()
     height,width = stdscr.getmaxyx()
     if height < height_min or width < width_min:
         # abort the program if the terminal can't be resized
         curses.endwin()
-        sys.exit('Please resize your terminal [%d%s%d] (minimum required 45x28)' %(width, 'x', height))
+        sys.exit('Please resize your terminal [%d%s%d] (minimum required %d%s%d)' %(width, 'x', height, width_min, 'x', height_min))
     curses.noecho()
     curses.cbreak()
     curses.curs_set(0)
